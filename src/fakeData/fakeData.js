d793ab9aef68738353fbe18289576e08f5abd956
rajibhossainprejon <rajibhossainpijeon@gmail.com>
2020-06-09 00:48:40 +0600

    2nd

---
+++ src/fakeData/fakeData.js
@@ -0,0 +1,99 @@
+
+var images=["../Breakfast/breakfast1.png","../Breakfast/breakfast2.png","../Breakfast/breakfast3.png","../Breakfast/breakfast4.png","../Breakfast/breakfast5.png","../Breakfast/breakfast6.png"];
+var data=[{ "img":images[0],
+      "userId": 1,
+      "id": 1,
+      "title": "quidem molestiae enim"
+    },
+    {"img":images[0],
+      "userId": 1,
+      "id": 2,
+      "title": "sunt qui excepturi placeat culpa"
+    },
+    {"img":images[0],
+      "userId": 1,
+      "id": 3,
+      "title": "omnis laborum odio"
+    },
+    {"img":images[0],
+      "userId": 1,
+      "id": 4,
+      "title": "non esse culpa molestiae omnis sed optio"
+    },
+    {"img":images[0],
+      "userId": 1,
+      "id": 5,
+      "title": "eaque aut omnis a"
+    },
+    {"img":images[0],
+      "userId": 1,
+      "id": 6,
+      "title": "natus impedit quibusdam illo est"
+    },
+    
+    {"img":images[0],
+      "userId": 2,
+      "id": 11,
+      "title": "quam nostrum impedit mollitia quod et dolor"
+    },
+    {"img":images[0],
+      "userId": 2,
+      "id": 12,
+      "title": "consequatur autem doloribus natus consectetur"
+    },
+    {"img":images[0],
+      "userId": 2,
+      "id": 13,
+      "title": "ab rerum non rerum consequatur ut ea unde"
+    },
+    {"img":images[0],
+      "userId": 2,
+      "id": 14,
+      "title": "ducimus molestias eos animi atque nihil"
+    },
+    {"img":images[0],
+      "userId": 2,
+      "id": 15,
+      "title": "ut pariatur rerum ipsum natus repellendus praesentium"
+    },
+    
+    {"img":images[0],
+      "userId": 2,
+      "id": 17,
+      "title": "aut minima voluptatem ut velit"
+    },
+    
+    {"img":images[0],
+      "userId": 3,
+      "id": 21,
+      "title": "repudiandae voluptatem optio est consequatur rem in temporibus et"
+    },
+    {"img":images[0],
+      "userId": 3,
+      "id": 22,
+      "title": "et rem non provident vel ut"
+    },
+    {"img":images[0],
+        "userId": 3,
+        "id": 23,
+        "title": "incidunt quisquam hic adipisci sequi"
+      },
+      {"img":images[0],
+        "userId": 3,
+        "id": 24,
+        "title": "dolores ut et facere placeat"
+      },
+      {"img":images[0],
+        "userId": 3,
+        "id": 25,
+        "title": "vero maxime id possimus sunt neque et consequatur"
+      },
+      {"img":images[0],
+        "userId": 3,
+        "id": 26,
+        "title": "quibusdam saepe ipsa vel harum"
+      }
+];
+
+
+export default data;
