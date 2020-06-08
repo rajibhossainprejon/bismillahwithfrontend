57819c38b00948df72bdd62d17f05504660672cc
rajibhossainprejon <rajibhossainpijeon@gmail.com>
2020-06-08 15:33:41 +0600

    2nd

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className="body ">
+            <section className="header">
+            
+            </section>
+                <section>
+
+                </section>
+                <section>
+
+                </section>
+            
+        </div>
+    );
+};
+
+export default Header;
